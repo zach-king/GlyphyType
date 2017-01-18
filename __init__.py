559dae6e1a18c277ba7978cae672a7686937ac79
Zachary King <kingzach77@gmail.com>
2017-01-18 08:41:59 -0600

    added docstring explaining __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+'''
+This file tells Python to treat the current directory (./GlyphyType/) as 
+a Python package. Hence, it allows us to `import GlyphyType` for use outside 
+of this application. This might be useful for extendinng GlyphyType in the future, 
+or simply for testing purposes. 
+
+Each .py file we create in this directory is a "module" and the string name should 
+be appended to the following list. 
+
+For example, if we had a "fontmanager.py" file, 
+we would have `__all__ = ['fontmanager']`
+'''
 __all__ = []
